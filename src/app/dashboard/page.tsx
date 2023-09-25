87eaf1bc6b04cb54f38163736c26fcb0b10685f9
Jakub Imiołczyk <j.prime153@yahoo.com>
2023-09-25 10:46:00 +0200

    Main code base for pwa

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Test } from "@/components/test";
+const Dashboard = () => {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+};
+
+export default Dashboard;
